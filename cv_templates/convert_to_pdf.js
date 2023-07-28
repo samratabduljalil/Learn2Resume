c547c7f759100743691e2c432dc372e620c0d7a1
abdurrahman4127 <whoamiiammr.nobody@gmail.com>
2023-07-28 23:20:12 +0600

    templete to pdf convertion is fking done!

---
+++ cv_templates/convert_to_pdf.js
@@ -0,0 +1,19 @@
+function convertHTMLtoPDF() {
+    let pdfjs = document.querySelector('#template_ar');
+    const { jsPDF } = window.jspdf;
+
+    let pdf_height = 1500; // in milimeter (mm)
+    let pdf_width = 1000;
+
+    let doc = new jsPDF('l', 'mm', [pdf_height, pdf_width]);
+    
+    doc.html(pdfjs, {
+        // content positions (in x-y direnction, milimeter)
+        x: 115,
+        y: 12,
+        
+        callback: function (doc) {
+            doc.save("template_ar.pdf");
+        }
+    });
+}
