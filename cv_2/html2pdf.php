23c395f302155afe936f47b632f7f339a496ceae
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2023-07-29 07:37:07 +0600

    new begining

---
+++ cv_2/html2pdf.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php'; // Assuming you have installed mPDF via Composer
+
+use Mpdf\Mpdf;
+ob_start();
+include('index.html');
+// HTML content to be converted to PDF
+$html = ob_get_contents();
+ob_end_clean();
+// Create a new mPDF instance
+$mpdf = new Mpdf();
+
+// (Optional) Set properties like font, orientation, and paper size
+$mpdf->SetTitle('HTML to PDF with mPDF');
+$mpdf->SetAuthor('Your Name');
+
+$mpdf->WriteHTML($html);
+
+// Save the generated PDF to a file
+$mpdf->Output();
+
+
+?>
