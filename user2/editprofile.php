704faf807d7a63f56d5344ad28d8b6468862defa
samratabduljalil <samratabduljalil21@gmail.com>
2023-08-22 17:26:37 +0600

    cover photo problem solved

--- user2/editprofile.php
+++ user2/editprofile.php
@@ -15,35 +15,35 @@
     <form action="" method="post">
         <p class="top_pro">Edit Your Personal Details</p>
         <textarea name="" id="" cols="30" rows="10" class="text_area" required>Write About You (in 50 word)</textarea>
-    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your name" required>
-    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your Phone Number" required>
-    <input type="email" name="" id="" class="text_box1" placeholder="Enter Your Email" required>
-    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your Address" required>
+    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your name" value="e">
+    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your Phone Number" value="e">
+    <input type="email" name="" id="" class="text_box1" placeholder="Enter Your Email" value="e">
+    <input type="text" name="" id="" class="text_box1" placeholder="Enter Your Address" value="e">
 
     <p class="top_pro">Edit Your Personal Details</p>
 
-    <input type="text" name="" id="" class="text_box1" placeholder='Instution Name* ' required>
+    <input type="text" name="" id="" class="text_box1" placeholder='Instution Name* ' value="e">
     
 
     <input type="text" name="" id="" class="text_box1" placeholder="Instution Name" >
-    <input type="text" name="" id="" class="text_box1" placeholder="Degree*" required>
+    <input type="text" name="" id="" class="text_box1" placeholder="Degree*" value="e">
      
        <input type="text" name="" id="" class="text_box1" placeholder="Degree" >
-       <input type="text" name="" id="" class="text_box1" placeholder="Obtain CGPA/GPA*" required>
+       <input type="text" name="" id="" class="text_box1" placeholder="Obtain CGPA/GPA*" value="e">
           
           <input type="text" name="" id="" class="text_box1" placeholder="Obtain CGPA/GPA" >
 
 
 
           <p class="top_pro">Works Exprience</p>
-          <input type="text" name="" id="" class="text_box1" placeholder="Organigetion  Name*" required>
+          <input type="text" name="" id="" class="text_box1" placeholder="Organigetion  Name*" value="e">
       
   
           <input type="text" name="" id="" class="text_box1" placeholder="Organigetion  Name" >
-          <input type="text" name="" id="" class="text_box1" placeholder="Position*" required>
+          <input type="text" name="" id="" class="text_box1" placeholder="Position*" value="e">
            
              <input type="text" name="" id="" class="text_box1" placeholder="Position" >
-             <input type="text" name="" id="" class="text_box1" placeholder="Working Year*" required>
+             <input type="text" name="" id="" class="text_box1" placeholder="Working Year*" value="e">
                 
                 <input type="text" name="" id="" class="text_box1" placeholder="Working Year" >
 
