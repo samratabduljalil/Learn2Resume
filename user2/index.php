6ba4734e532f40fedf03231e3751a3632408aec7
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2023-08-23 06:37:50 +0600

    course game

--- user2/index.php
+++ user2/index.php
@@ -20,6 +20,7 @@
   
     <li><a href="\chosetemplate.php">Create Cv</a></li>
     <li><a href="editprofile.php">Edit Profile</a></li>
+    <li><a href="\user\index.html">Your Courses</a></li>
     <li><a href="">All Courses</a></li>
     <li><a href="\index.php">Home</a></li>
     <li><a href="\logout.php">Logout</a></li>
