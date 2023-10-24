2cd308de13c61fe42b53954e05f1c59cd5cab006
samratabduljalil <samratabduljalil21@gmail.com>
2023-10-25 00:04:48 +0600

    working on api

--- course/fetcheAllData_api.php
+++ course/fetcheAllData_api.php
@@ -1,7 +1,29 @@
 <?php
 
 include('connection.php');
+header('Content-Type: application/json');
+// SQL query to fetch data from the database
+$sql = "SELECT * FROM course";
 
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    // Close the database connection
+    $conn->close();
+
+    // Set the response content type to JSON
+  
+
+    // Return the data as JSON
+    echo json_encode($data);
+} else {
+    echo json_encode("No data found");
+}
+?>
 
 
 
