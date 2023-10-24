82b0318159a672ecec3f357830d4e5eedb50383d
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2023-10-25 00:52:11 +0600

    course API

--- course/fetcheAllData_api.php
+++ course/fetcheAllData_api.php
@@ -1,7 +1,7 @@
 <?php
 
 include('connection.php');
-header('Content-Type: application/json');
+
 // SQL query to fetch data from the database
 $sql = "SELECT * FROM course";
 
@@ -16,7 +16,7 @@ if ($result->num_rows > 0) {
     $conn->close();
 
     // Set the response content type to JSON
-  
+    header('Content-Type: application/json');
 
     // Return the data as JSON
     echo json_encode($data);
