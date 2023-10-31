993832505919fd41d44364dafb67b8cd66ac0566
samratabduljalil <samratabduljalil21@gmail.com>
2023-10-31 08:17:59 +0600

    work on add course api

--- admin/add_c.php
+++ admin/add_c.php
@@ -3,44 +3,28 @@
 include('connection.php');
 session_start();
 
-$course_name = $_POST["course_name"];
-$course_code = $_POST['course_code'];
+$data = json_decode(file_get_contents("php://input"), true);
 
+// Insert the product into the database
+$Name = $data['course_name'];
+$code = $data['course_code'];
+$fee = $data['coursefee'];
+$img = $data['img'];
 
+$sql = "INSERT INTO `course`( `Name`, `course_code`, `img`, `fee`) VALUES ('{$Name}','{$code}','{$img}','{$fee}')";
 
-
-$query = "INSERT INTO `course`( `Name`, `course_code`) VALUES ('{$course_name}', '{$course_code}')";
-
-
-if (mysqli_query($connection, $query)) {
-    header("location: addcourse.php");
-    $_SESSION['done']=True;
- } else {
-    echo "Error inserting data: " . mysqli_error($connection);
+if ($connection->query($sql) === TRUE) {
+    $response = ['message' => 'Course Added successfully.'];
+  
+} else {
+    $response = ['message' => 'Error: ' . $connection->error];
 }
 
+// Close the database connection
+$connection->close();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+header("Content-Type: application/json");
+echo json_encode($response);
 
 
 
--- admin/addcourse.php
+++ admin/addcourse.php
@@ -15,26 +15,16 @@
     
 <div class="con">
     <div class="design2">
-    <form action="add_c.php" method="post" enctype="multipart/form-data" >
+    <form id="courseForm" enctype="multipart/form-data" >
         <p class="top_pro">ADD New Course</p>
-        <?php if(isset($_SESSION['done'])){
-        if($_SESSION['done']==True) {?>
-            <p class="top_pro1">sucessfully added</p>
-            <input type="text" name="course_name" id="" class="text_box1" placeholder="Enter Course name" required>
-    <input type="text" name="course_code" id="" class="text_box1" placeholder="Enter Your Course code" required>
-    <input type="file" name="img" id="img" >
+        
+            <p class="top_pro1" id="message"></p>
+            <input type="text" id="course_name" id="" class="text_box1" placeholder="Enter Course name" required>
+    <input type="text" id="course_code" id="" class="text_box1" placeholder="Enter Your Course code" required>
+    <input type="text" id="course_fee" id="" class="text_box1" placeholder="Enter Your Course Fee" required>
+    <input type="file" id="img"  >
 
-        <?php $_SESSION['done']=False ;
-    }else{?>
-    <input type="text" name="course_name" id="" class="text_box1" placeholder="Enter Course name" required>
-    <input type="text" name="course_code" id="" class="text_box1" placeholder="Enter Your Course code" required>
-    <input type="file" name="img" id="img" >
-
-    <?php }}else{?>
-        <input type="text" name="course_name" id="" class="text_box1" placeholder="Enter Course name" required>
-    <input type="text" name="course_code" id="" class="text_box1" placeholder="Enter Your Course code" required>
-    <input type="file" name="img" id="img" >
-        <?php }?>
+   
         
             <button class="btn_submit" type="submit">Add course</button>
             
@@ -42,6 +32,51 @@
    
 </div>
 </div>
+
+
+
+
+
+
 <a href="index.php" ><button class="btn_submit"  >Back</button></a>
+
+<script>
+        document.getElementById("courseForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            // Get form data
+            const course_name = document.getElementById("course_name").value;
+            const course_code = document.getElementById("course_code").value;
+            const coursefee = document.getElementById("coursefee").value;
+            const img = document.getElementById("img").value;
+
+            // Create an object with the form data
+            const data = {
+                course_name,
+                course_code,
+                coursefee,
+                img
+            };
+
+            // Send the data to the server
+            fetch("add_c.php", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(result => {
+                document.getElementById("message").textContent = result.message;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        });
+    </script>
+
+
+
 </body>
 </html>
